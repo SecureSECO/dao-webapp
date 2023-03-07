d02f1b66ecc8d7a8fb308d652c3f308d135c5cd2
Hidde-Heijnen <hiddeh11@gmail.com>
2023-03-07 14:29:38 +0100

    dotenv

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -23,5 +23,9 @@ module.exports = {
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
   importOrderMergeDuplicateImports: true,
   importOrderCombineTypeAndValueImports: true,
-  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  plugins: [
+    require("@ianvs/prettier-plugin-sort-imports"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+  pluginSearchDirs: false,
 }
