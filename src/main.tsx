4db30f7cfda6539059cd7d15e205d66d5d08575d
Hidde-Heijnen <hiddeh11@gmail.com>
2023-03-09 17:01:56 +0100

    chore: formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import {
   EthereumClient,
   modalConnectors,
   walletConnectProvider,
-} from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import { configureChains, createClient, WagmiConfig } from 'wagmi'
-import { goerli, polygon } from 'wagmi/chains'
+} from '@web3modal/ethereum';
+import { Web3Modal } from '@web3modal/react';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { goerli, polygon } from 'wagmi/chains';
 
 // 1. Get projectID at https://cloud.walletconnect.com
 if (!process.env.REACT_APP_PROJECT_ID) {
-  throw new Error('You need to provide REACT_APP_PROJECT_ID env variable')
+  throw new Error('You need to provide REACT_APP_PROJECT_ID env variable');
 }
-const projectId = process.env.REACT_APP_PROJECT_ID
+const projectId = process.env.REACT_APP_PROJECT_ID;
 
 // 2. Configure wagmi client
-const chains = [polygon, goerli]
+const chains = [polygon, goerli];
 
 const { provider } = configureChains(chains, [
   walletConnectProvider({ projectId }) as any,
-])
+]);
 const wagmiClient = createClient({
   autoConnect: true,
   connectors: modalConnectors({ appName: 'SecureSecoDao', chains }) as any,
   provider,
-})
+});
 
 // 3. Configure modal ethereum client
-const ethereumClient = new EthereumClient(wagmiClient, chains)
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
-import App from './App'
-import './index.css'
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -42,4 +42,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 
     <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
   </React.StrictMode>
-)
+);
