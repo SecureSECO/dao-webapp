f057051d39589620da30fc7d21af236be7175596
Thinus <thijnkroon@gmail.com>
2023-03-13 20:44:54 +0100

    feat: button component styling

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,21 +4,21 @@ import { VariantProps, cva } from 'class-variance-authority';
 import { cn } from '../../lib/utils';
 
 const buttonVariants = cva(
-  'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-default dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900',
+  'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-default disabled:pointer-events-none',
   {
     variants: {
       variant: {
         default:
-          'bg-primary text-white hover:bg-primary-500 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-200 focus:ring-primary-400', // for button that opens some menu: data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800
+          'bg-primary text-white hover:bg-primary-500 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-200 focus:ring-primary-400 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900', // for button that opens some menu: data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800
         destructive:
-          'bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600 focus:ring-red-400',
+          'bg-red-500 text-white hover:bg-red-600  focus:ring-red-400',
         outline:
-          'bg-transparent border border-slate-200 hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100',
+          'bg-transparent border border-slate-300 hover:bg-slate-100 dark:border-slate-600 dark:hover:bg-slate-800 dark:text-slate-100 focus:ring-primary-200',
         subtle:
-          'bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100',
+          'bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:hover:bg-slate-800 dark:text-slate-100 focus:ring-primary-200',
         ghost:
-          'bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 dark:hover:text-slate-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent',
-        link: 'bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent',
+          'bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 focus:ring-primary-200',
+        link: 'bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 focus:ring-primary-200 underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-10 py-2 px-4',
