8f9cd0045adbd3c5531b6ca85a0f375e2252b4d1
BenjaminMeijs <benjamin.meijs@gmail.com>
2023-03-14 16:33:09 +0100

    fix(106): Solve common js compatibility issue

---
+++ .storybook/main.cjs
@@ -0,0 +1,25 @@
+// BUG: Storybook-test does not work if this file is a typescript file.
+// FIX: We turned this file into a .cjs (commonJS) file.
+// References: https://github.com/storybookjs/storybook/issues/11587, https://github.com/sveltejs/kit/discussions/2803
+
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  core:{
+    disableTelemetry: true,
+  }
+};
+
+
+module.exports = config;
