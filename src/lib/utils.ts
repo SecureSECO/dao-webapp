508f50676bb62bc8150ef1cb4fc3d36c1cb6ceea
BenjaminMeijs <benjamin.meijs@gmail.com>
2023-03-16 11:50:54 +0100

    docs: Add doc comment to cn function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Utility function for combining html Class Names (CN).
+ * For conflicting styles, the 'latest' style is used.
+ *
+ * @remarks
+ * This function is a combination of clsx and and tailwindMerge
+ *
+ * @param inputs - A list of 'rich' class names. See clsx documentation for what 'rich' means
+ * @returns Optimized class name string
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
