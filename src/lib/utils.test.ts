1148f5b61d1a19f3f53aae0b2e2bd84c66207868
BenjaminMeijs <benjamin.meijs@gmail.com>
2023-03-16 13:01:47 +0100

    test: Add another simple test

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,7 +1,12 @@
 import { cn } from './utils';
 
+// These are very simplistic tests, mostly meant as example tests.
+
 test('cn with padding conflicts uses last padding', () => {
-  const className = 'p-0 p-32';
-  const expected = 'p-32';
-  expect(cn(className)).toBe(expected);
+  expect(cn('p-0 p-32')).toBe('p-32');
+  expect(cn('p-0', 'p-32')).toBe('p-32');
+});
+
+test('cn with simple && returns correct', () => {
+  expect(cn('p-0', false && 'm-1', true && 'grow-0')).toBe('p-0 grow-0');
 });
