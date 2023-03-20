d466109dd6973b658f9d9d3f8d8042f055e5ae12
Hidde-Heijnen <hiddeh11@gmail.com>
2023-03-20 23:25:08 +0100

    feat: ugly new proposal start

--- src/components/ui/Progress.tsx
+++ src/components/ui/Progress.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import * as ProgressPrimitive from '@radix-ui/react-progress';
 
-import { cn } from '@/lib/utils';
+//TODO might need updated colors or different variations
+
+import { cn } from '@/src/lib/utils';
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
@@ -10,13 +12,13 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      'relative h-4 w-full overflow-hidden rounded-full bg-slate-200 dark:bg-slate-800',
+      'relative h-4 w-full overflow-hidden rounded-full bg-slate-200 dark:bg-slate-950',
       className
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-slate-900 transition-all dark:bg-slate-400"
+      className="h-full w-full flex-1 bg-primary transition-all dark:bg-primary-500"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
