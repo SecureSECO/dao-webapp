f5884ce1014b050448aa75855045d58b04a02058
BenjaminMeijs <benjamin.meijs@gmail.com>
2023-03-23 16:34:10 +0100

    feat: abbreviated token amounts

--- src/components/ui/TokenAmount/TokenAmount.stories.tsx
+++ src/components/ui/TokenAmount/TokenAmount.stories.tsx
@@ -14,3 +14,29 @@ export const Primary: Story = {
     <TokenAmount amount={1234556789n} tokenDecimals={6} symbol="TEST" />
   ),
 };
+
+export const LowAmount: Story = {
+  render: () => (
+    <TokenAmount amount={1234556789n} tokenDecimals={14} symbol="TEST" />
+  ),
+};
+
+export const HighAmount: Story = {
+  render: () => (
+    <TokenAmount
+      amount={3123456789123456789n}
+      tokenDecimals={8}
+      symbol="Test"
+    />
+  ),
+};
+
+export const TooHighAmount: Story = {
+  render: () => (
+    <TokenAmount
+      amount={3123456789123456789789123n}
+      tokenDecimals={8}
+      symbol="Test"
+    />
+  ),
+};
--- src/components/ui/TokenAmount/TokenAmount.tsx
+++ src/components/ui/TokenAmount/TokenAmount.tsx
@@ -18,6 +18,52 @@ export const bigIntToFloat = (
   onError: string = '-'
 ): number => parseFloat(value && decimals ? `${value}E-${decimals}` : onError);
 
+// Taken from Aragon App
+export function abbreviateTokenAmount(amount: string): string {
+  if (!amount) return 'N/A';
+
+  const TOKEN_AMOUNT_REGEX =
+    /(?<integers>[\d*]*)[.]*(?<decimals>[\d]*)\s*(?<symbol>[A-Za-z]*)/;
+  const regexp_res = amount.match(TOKEN_AMOUNT_REGEX);
+
+  // discard failed matches
+  if (regexp_res?.length !== 4 || regexp_res[0].length !== amount.length)
+    return 'N/A';
+
+  // retrieve capturing groups
+  const integers = regexp_res[1];
+  const decimals = regexp_res[2];
+  const symbol = regexp_res[3];
+
+  if (integers?.length > 4) {
+    const integerNumber = Number.parseInt(integers);
+    const magnitude = Math.floor((integers.length - 1) / 3);
+    const lead = Math.floor(integerNumber / Math.pow(10, magnitude * 3));
+    const magnitude_letter = ['k', 'M', 'G'];
+
+    return `${lead}${
+      magnitude < 4
+        ? magnitude_letter[magnitude - 1]
+        : '*10^' + Math.floor(magnitude) * 3
+    }${symbol && ' ' + symbol}`;
+  }
+
+  if (decimals) {
+    const fraction = '0.' + decimals;
+    const fractionNumber = Number.parseFloat(fraction);
+    const intNumber = Number.parseInt(integers);
+    const totalNumber = intNumber + fractionNumber;
+
+    if (totalNumber < 0.01) {
+      return ` < 0.01${symbol && ' ' + symbol}`;
+    }
+
+    return `${totalNumber.toFixed(2)}${symbol && ' ' + symbol}`;
+  }
+
+  return `${Number.parseInt(integers)}${symbol && ' ' + symbol}`;
+}
+
 /**
  * @param className - ClassName to pass
  * @param amount - The amount of the given token, default value is 1
@@ -26,7 +72,6 @@ export const bigIntToFloat = (
  * @param symbol - The symbol of the token, e.g. LINK, ETH, JSCL. Default is empty string.
  * @param sign - optional sign. Default value '' (empty string). Recommended values: '' (empty string), + (plus) or - (minus).
  * @param onError - Value to display if something went wrong. Default value '-'.
- * @param significanceDecimals - Amount of decimals to display. Default value 2. E.g. 1.23
  */
 const TokenAmount = ({
   className = '',
@@ -35,13 +80,12 @@ const TokenAmount = ({
   symbol,
   sign = '',
   onError = '-',
-  displayDecimals = 2,
 }: TokenAmountProps) => {
   return (
     <span className={className}>
       {sign}
-      {bigIntToFloat(amount ?? 1n, tokenDecimals ?? 0, onError).toFixed(
-        displayDecimals
+      {abbreviateTokenAmount(
+        bigIntToFloat(amount ?? 1n, tokenDecimals ?? 0, onError).toFixed(2)
       )}
       &nbsp;
       {symbol ?? ''}
