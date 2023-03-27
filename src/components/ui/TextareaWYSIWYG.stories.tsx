dc1ef0d4ff81f1e69d0699b6d56096c978b8d7d6
BenjaminMeijs <benjamin.meijs@gmail.com>
2023-03-27 16:07:12 +0200

    feat: Add TextareaWYSIWYG story

---
+++ src/components/ui/TextareaWYSIWYG.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { TextareaWYSIWYG } from './TextareaWYSIWYG';
+
+const meta: Meta<typeof TextareaWYSIWYG> = {
+  component: TextareaWYSIWYG,
+};
+
+export default meta;
+type Story = StoryObj<typeof TextareaWYSIWYG>;
+
+export const Primary: Story = {
+  render: () => <TextareaWYSIWYG />,
+};
