06825760cdca6cdd167f64b59764fecad5a03416
BenjaminMeijs <benjamin.meijs@gmail.com>
2023-04-03 17:55:51 +0200

    chore: Improve mintTokensAction

--- src/components/proposal/MintTokensAction.tsx
+++ src/components/proposal/MintTokensAction.tsx
@@ -1,6 +1,14 @@
-import { ActionMintToken } from '@/src/lib/Actions';
+import {
+  Action,
+  ActionMintToken,
+  EmptyActionMintToken,
+} from '@/src/lib/Actions';
+import { AddressPattern, NumberPattern } from '@/src/lib/Patterns';
 import { Input } from '../ui/Input';
-import { Label } from '../ui/Label';
+import { HiPlus, HiXMark } from 'react-icons/hi2';
+import { Button } from '../ui/Button';
+import { useState } from 'react';
+import { Card } from '../ui/Card';
 
 export const MintTokensAction = ({
   action,
@@ -11,18 +19,91 @@ export const MintTokensAction = ({
   register: any;
   prefix: string;
 }) => {
-  return <div className="flex flex-col gap-4"></div>;
+  const [_action, setAction] = useState<ActionMintToken>(action);
+  const handleAddMintTokensAction = () => {
+    setAction({
+      ..._action,
+      inputs: {
+        mintTokensToWallets: [
+          ..._action.inputs.mintTokensToWallets,
+          { address: '', amount: '' },
+        ],
+      },
+    });
+  };
+
+  const handleRemove = (index: number) => {
+    const newMintTokens = _action.inputs.mintTokensToWallets.slice(0);
+    newMintTokens.splice(index, 1);
+    setAction({ ..._action, inputs: { mintTokensToWallets: newMintTokens } });
+  };
+
+  return (
+    <Card className="grid grid-cols-3 gap-4">
+      <span className="col-span-1 pl-2">Address</span>
+      <span className="col-span-1 pl-2">Tokens</span>
+      <span className="col-span-1" />
+      {/* TODO: Fix removal of elements */}
+      {_action.inputs.mintTokensToWallets.map((atm, index) => (
+        <AddressTokensMint
+          addressToken={atm}
+          register={register}
+          prefix={`${prefix}.${index}`}
+          onRemove={() => handleRemove(index)}
+        />
+      ))}
+      <Button
+        variant="subtle"
+        type="button"
+        label="Add wallet"
+        icon={HiPlus}
+        onClick={handleAddMintTokensAction}
+      />
+    </Card>
+  );
 };
 
-const AdressTokensMint = ({ onChange, onRemove, register, prefix }: any) => (
-  <div className="flex flex-col gap-4">
+type AddressToken = {
+  address: string;
+  amount: string | number;
+};
+
+const AddressTokensMint = ({
+  addressToken,
+  register,
+  onRemove,
+  prefix,
+}: {
+  addressToken: AddressToken;
+  register: any;
+  onRemove: () => void;
+  prefix: string;
+}) => (
+  <div className="col-span-3 grid grid-cols-3 gap-4">
     <div className="flex flex-col gap-2">
-      <Label htmlFor="address">Address</Label>
-      <Input {...register(`${prefix}.address`)} type="text" id="address" />
+      {/* <Label htmlFor="address">Address</Label> */}
+      {/* TODO Add pattern for validation */}
+      <Input
+        {...register(`${prefix}.address`)}
+        type="text"
+        id="address"
+        defaultValue={addressToken.address}
+        pattern={AddressPattern}
+      />
     </div>
     <div className="flex flex-col gap-2">
-      <Label htmlFor="tokens">Token Amount</Label>
-      <Input {...register(`${prefix}.tokens`)} type="number" id="tokens" />
+      {/* <Label htmlFor="tokens">Tokens</Label> */}
+      <Input
+        {...register(`${prefix}.tokens`)}
+        type="number"
+        id="tokens"
+        defaultValue={addressToken.amount}
+        pattern={NumberPattern}
+      />
     </div>
+    <HiXMark
+      className="h-5 w-5 cursor-pointer self-center"
+      onClick={onRemove}
+    />
   </div>
 );
--- src/components/proposal/ProposalActionList.stories.tsx
+++ src/components/proposal/ProposalActionList.stories.tsx
@@ -1,4 +1,8 @@
-import { EmptyActionMintToken, emptyActionWithdraw } from '@/src/lib/Actions';
+import {
+  Action,
+  EmptyActionMintToken,
+  emptyActionWithdraw,
+} from '@/src/lib/Actions';
 import type { Meta, StoryObj } from '@storybook/react';
 import { useForm } from 'react-hook-form';
 
@@ -11,9 +15,7 @@ const meta: Meta<typeof ProposalActionList> = {
 export default meta;
 type Story = StoryObj<typeof ProposalActionList>;
 
-const actions = [emptyActionWithdraw, EmptyActionMintToken];
-
-export const Primary: Story = {
+const StoryBuilder = (actions: Action[]): Story => ({
   render: () => {
     const { register, control } = useForm();
     return (
@@ -26,4 +28,28 @@ export const Primary: Story = {
       </form>
     );
   },
-};
+});
+
+const emptyActions = [emptyActionWithdraw, EmptyActionMintToken];
+export const EmptyActions: Story = StoryBuilder(emptyActions);
+
+const mintTokensAction: Action[] = [
+  {
+    name: 'mint_tokens',
+    inputs: {
+      mintTokensToWallets: [
+        { address: '0x123456789', amount: 5 },
+        { address: '0x987654321', amount: 3 },
+        { address: '0x111222233', amount: 8 },
+      ],
+    },
+    summary: {
+      newTokens: 122,
+      tokenSupply: 3,
+      newHoldersCount: 8,
+      daoTokenSymbol: 'REP',
+      daoTokenAddress: '0x123456789',
+    },
+  },
+];
+export const MintTokensAction = StoryBuilder(mintTokensAction);
