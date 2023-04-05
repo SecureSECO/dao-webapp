2a47dc99789aca6619ffa1b0e40f2000a35c01ea
Hidde Heijnen <h.r.a.heijnen@students.uu.nl>
2023-04-05 23:13:47 +0200

    docs: sortselector comments

--- src/components/ui/Progress.tsx
+++ src/components/ui/Progress.tsx
@@ -1,6 +1,7 @@
 /**
  * The Progress module provides a customizable progress bar component.
  * It supports different sizes and variants, and can be used to display the progress of an operation.
+ *  Inspired, but adapted from: https://ui.shadcn.com/docs/primitives/progress
  */
 
 import * as React from 'react';
