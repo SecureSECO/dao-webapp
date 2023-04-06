b2f5497348950173aebcfdf0d52f6821c47c8ca9
Hidde-Heijnen <hiddeh11@gmail.com>
2023-04-06 09:05:12 +0200

    docs: Toggle comments

--- src/components/ui/Toggle.tsx
+++ src/components/ui/Toggle.tsx
@@ -1,9 +1,18 @@
+/**
+ * @file Custom Toggle Component
+ * This file defines a custom toggle component that extends the functionality of the Radix UI toggle primitive.
+ * It includes the ability to use custom variants and sizes, and applies custom styling using the `class-variance-authority` package.
+ * @see https://www.radix-ui.com/docs/primitives/components/toggle - Radix UI Toggle Primitive
+ * This component was inspired by https://ui.shadcn.com/docs/primitives/toggle
+ */
+
 import * as React from 'react';
 import * as TogglePrimitive from '@radix-ui/react-toggle';
 import { VariantProps, cva } from 'class-variance-authority';
 
 import { cn } from '@/src/lib/utils';
 
+// Define custom toggle styling and variants using class-variance-authority
 const toggleVariants = cva(
   'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors data-[state=on]:bg-slate-200 dark:hover:bg-slate-800 dark:data-[state=on]:bg-slate-700 focus:outline-none dark:text-slate-100 focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:focus:ring-offset-slate-900 hover:bg-slate-100  dark:hover:text-slate-100 dark:data-[state=on]:text-slate-100',
   {
@@ -26,6 +35,14 @@ const toggleVariants = cva(
   }
 );
 
+/**
+ * Custom Toggle component that supports variants and sizes, and applies custom styling.
+ *
+ * @param {object} props - The properties for the toggle component.
+ * @param {React.Ref} ref - The forwarded ref for the TogglePrimitive.Root.
+ *
+ * @returns {React.Element} The rendered Toggle component.
+ */
 const Toggle = React.forwardRef<
   React.ElementRef<typeof TogglePrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> &
