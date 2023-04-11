32b4cbc063cf180ea32687bcdf67f59f4de66bd6
BenjaminMeijs <benjamin.meijs@gmail.com>
2023-04-11 16:00:51 +0200

    feat: add tests for patterns

--- src/lib/Patterns.ts
+++ src/lib/Patterns.ts
@@ -6,5 +6,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+/**
+ * NumberPattern is a pattern for positive floats. See the test cases for examples
+ */
 export const NumberPattern = '\\d+.?\\d*';
+
+/**
+ * AddressPattern is a pattern for Addresses, starting with 0x, followed by 20 to 60 hex characters.
+ * See the test cases for examples.
+ */
 export const AddressPattern = '0x[a-fA-F0-9]{20,60}';
---
+++ src/lib/patterns.test.ts
@@ -0,0 +1,69 @@
+import { AddressPattern, NumberPattern } from './patterns';
+
+test('Success cases for Number Pattern', () => {
+  const successes = [
+    '1',
+    '3',
+    '012345678900987654321',
+    '0987654321234567890.0987654321234567890',
+    '1.0',
+    '3.14159',
+    '0987654321234567890.0987654321234567890',
+  ];
+  testSuccesses(NumberPattern, successes);
+});
+
+test('Failure cases for Number Pattern', () => {
+  const failures = [
+    'Not a number',
+    'NaN',
+    '-1',
+    '-3.14159',
+    ' 2',
+    '3 ',
+    '',
+    '1 3',
+  ];
+  testFailures(NumberPattern, failures);
+});
+
+test('Success cases for Address Pattern', () => {
+  const successes = [
+    '0xf80A4cbdA4Fb35d59B40d9aD54C198280bBa1B36',
+    '0xED45FF9490723c2fb4A354e4B554c357604eA73C',
+    '0xdAfAB237B65cf37B3aAB7aF251C99764998e5E97',
+    '0xaF7E68bCb2Fc7295492A00177f14F59B92814e70',
+    '0x7f2B04dFE3a529BB6527cF4C33F87F5A87b4AB2f',
+  ];
+  testSuccesses(AddressPattern, successes);
+});
+
+test('Failure cases for Address Pattern', () => {
+  const failures = [
+    'Not an address',
+    '7f2B04dFE3a529BB6527cF4C33F87F5A87b4AB2f', // No 0x leader
+    'null',
+    '', //empty string
+    ' 0x7f2B04dFE3a529BB6527cF4C33F87F5A87b4AB2f', //space at beginning
+    '0x7f2B04dFE3a529BB6527c F4C33F87F5A87b4AB2f', //space at middle
+    '0x7f2B04dFE3a529BB6527cF4C33F87F5A87b4AB2f ', // space at end
+    '0x7f2B04dFE3a529BB6527cF4C33F87F5A87b4AB2f7f2B04dFE3a529BB6527cF4C33F87F5A87b4AB2f', // too long
+    '0x7f2B04', // too short
+    '0x7f2z04dZE3a529BB6z27cF4C33FZ7F5A87b4zB2f', //contains z and Z
+  ];
+  testFailures(AddressPattern, failures);
+});
+
+function testSuccesses(pattern: string, successes: string[]) {
+  const regex = new RegExp(pattern);
+  successes.forEach((value) => {
+    expect(regex.test(value)).toBeTruthy;
+  });
+}
+
+function testFailures(pattern: string, failures: string[]) {
+  const regex = new RegExp(pattern);
+  failures.forEach((value) => {
+    expect(regex.test(value)).toBeFalsy;
+  });
+}
