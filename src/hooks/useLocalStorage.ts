f6e44aa1bb8c620a9301b5989125f77f98e652ee
Mike <23509561+mik-0@users.noreply.github.com>
2023-04-28 15:29:04 +0200

    chore: add license

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,3 +1,11 @@
+/**
+ * This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
+ * © Copyright Utrecht University (Department of Information and Computing Sciences)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // SOURCE: https://usehooks.com/useLocalStorage/
 // Sync state to local storage so that it persists through a page refresh. Usage is similar to useState except we pass
 //  in a local storage key so that we can default to that value on page load instead of the specified initial value.
