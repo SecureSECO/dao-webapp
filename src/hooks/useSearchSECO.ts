6e4787dc1032b9763bc44bcacf5eeb967a3b54ca
6627811 <ras.roderick@gmail.com>
2023-05-02 13:14:00 +0200

    chore: add license

--- src/hooks/useSearchSECO.ts
+++ src/hooks/useSearchSECO.ts
@@ -1,3 +1,10 @@
+/**
+ * This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
+ * © Copyright Utrecht University (Department of Information and Computing Sciences)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 import { useEffect, useState } from 'react';
 import { getErrorMessage } from '@/src/lib/utils';
