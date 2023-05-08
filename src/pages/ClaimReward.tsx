78ead97fd926b2c22286568cba3f47a8cb472c67
BenjaminMeijs <benjamin.meijs@gmail.com>
2023-05-08 17:41:19 +0200

    feat(claimRewards): improve styling

--- src/pages/ClaimReward.tsx
+++ src/pages/ClaimReward.tsx
@@ -15,6 +15,10 @@ import { Label } from '../components/ui/Label';
 import { Slider } from '../components/ui/Slider';
 import { Button } from '../components/ui/Button';
 import TokenAmount from '../components/ui/TokenAmount';
+import { MainCard } from '../components/ui/MainCard';
+import { IconBaseProps } from 'react-icons/lib';
+import { HiOutlineCurrencyDollar } from 'react-icons/hi2';
+import { Card } from '../components/ui/Card';
 
 /*
  * repToBeClaimed is the total amount of reputation the user may claim
@@ -52,51 +56,52 @@ export const ClaimReward = ({
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-      <div className="flex w-full flex-row gap-x-4">
-        <div className="flex flex-col gap-y-1">
-          Reputation
-          <span>{division}%</span>
+    <MainCard icon={HiOutlineCurrencyDollar} header="Claim Reward">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+        <Label
+          htmlFor="division"
+          tooltip="How to divide the reward between monetary value and repuation"
+        >
+          Reward division
+        </Label>
+        <Card
+          variant="outline"
+          className="grid w-full grid-cols-8 flex-col gap-x-4 gap-y-4 text-center"
+        >
+          <div className="col-span-4 flex flex-col gap-y-1 md:col-span-1">
+            Reputation
+            <span>{division}%</span>
+          </div>
+          <div className="col-span-4 flex flex-col gap-y-1 md:order-last md:col-span-1">
+            Monetary
+            <span>{100 - division}%</span>
+          </div>
+          <div className="col-span-8 flex flex-col justify-center gap-y-2 md:col-span-6">
+            <Controller
+              control={control}
+              name={name_division}
+              render={({ field: { onChange, name, value } }) => (
+                <Slider
+                  defaultValue={[value]}
+                  max={100}
+                  step={1}
+                  onValueChange={onChange}
+                  name={name}
+                />
+              )}
+            />
+          </div>
+        </Card>
+        <div className="flex w-full flex-col gap-x-5 gap-y-2 md:flex-row">
+          <Card variant="outline">
+            <TokenAmount amountFloat={reputation} symbol="reputation token" />
+          </Card>
+          <Card variant="outline">
+            <TokenAmount amountFloat={monetary} symbol="monetary token" />
+          </Card>
         </div>
-        <div className="flex w-full flex-col gap-y-2">
-          <Label
-            htmlFor="division"
-            className="justify-center text-center"
-            tooltip="How to divide the reward between monetary value and repuation"
-          >
-            Division
-          </Label>
-          <Controller
-            control={control}
-            name={name_division}
-            render={({ field: { onChange, name, value } }) => (
-              <Slider
-                defaultValue={[value]}
-                max={100}
-                step={1}
-                onValueChange={onChange}
-                name={name}
-              />
-            )}
-          />
-        </div>
-        <div className="flex flex-col gap-y-1"></div>
-        <div className="flex flex-col gap-y-1">
-          Monetary
-          <span>{100 - division}%</span>
-        </div>
-      </div>
-      <div>
-        You can claim:{' '}
-        <span className="font-bold">
-          <TokenAmount amountFloat={reputation} symbol="reputation token" />
-        </span>{' '}
-        and{' '}
-        <span className="font-bold">
-          <TokenAmount amountFloat={monetary} symbol="monetary token" />
-        </span>
-      </div>
-      <Button label="Claim" />
-    </form>
+        <Button label="Claim reward" />
+      </form>
+    </MainCard>
   );
 };
