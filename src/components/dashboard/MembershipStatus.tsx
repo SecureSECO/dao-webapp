28151a5d7f9397651b6e282e66b3f08da0f391bf
BenjaminMeijs <benjamin.meijs@gmail.com>
2023-05-09 11:55:38 +0200

    feat: add network check to membershipStatus

--- src/components/dashboard/MembershipStatus.tsx
+++ src/components/dashboard/MembershipStatus.tsx
@@ -6,22 +6,45 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { Verification } from '@/src/hooks/useVerification';
+import { Verification, useVerification } from '@/src/hooks/useVerification';
 import { useWeb3Modal } from '@web3modal/react';
 import { addDays } from 'date-fns';
 import { HiOutlineExclamationCircle } from 'react-icons/hi2';
 import { Card } from '../ui/Card';
 import { Link as RouterLink } from 'react-router-dom';
+import { Chain, useAccount, useNetwork, useSwitchNetwork } from 'wagmi';
+import { PREFERRED_NETWORK_METADATA } from '@/src/lib/constants/chains';
 
-export const MembershipStatus = ({
+export const MembershipStatus = () => {
+  const { open } = useWeb3Modal();
+  const { isConnected } = useAccount();
+  const { memberVerification } = useVerification({ useDummyData: true });
+  const { chain } = useNetwork();
+  const { switchNetwork } = useSwitchNetwork();
+
+  return (
+    <MembershipStatusView
+      isConnected={isConnected}
+      verification={memberVerification}
+      chainId={chain?.id}
+      openConnector={open}
+    />
+  );
+};
+
+export const MembershipStatusView = ({
   isConnected,
   verification,
+  chainId,
+  openConnector,
+  switchNetwork,
 }: {
-  isConnected: Boolean;
+  isConnected: boolean;
   verification: Verification;
+  chainId?: number;
+  openConnector: (options?: any | undefined) => Promise<void>;
+  switchNetwork?: (chainId_?: number) => void;
 }) => {
-  const { open } = useWeb3Modal();
-
   // If user has not connected a wallet:
   // An informative banner, with button to connect wallet
   if (!isConnected) {
@@ -35,10 +58,36 @@ export const MembershipStatus = ({
         <button
           type="button"
           className="rounded-sm text-primary-highlight underline ring-ring ring-offset-2 ring-offset-destructive-background transition-colors duration-200 hover:text-primary-highlight/80 focus:outline-none focus:ring-1"
-          onClick={() => open()}
+          onClick={() => openConnector()}
         >
           Connect Wallet.
         </button>
+      </Card>
+    );
+  }
+
+  // If user has connected a wallet, but the wallet is not on the correct network:
+  // An informative banner, showing a button to change network.
+
+  if (chainId !== undefined && chainId !== PREFERRED_NETWORK_METADATA.id) {
+    return (
+      <Card
+        variant="warning"
+        className="col-span-full flex flex-row items-center gap-x-1"
+      >
+        <HiOutlineExclamationCircle className="h-5 w-5 shrink-0" />
+        Your wallet network is not correct{switchNetwork ? ':' : '.'}
+        {switchNetwork ? (
+          <button
+            type="button"
+            className="rounded-sm text-primary-highlight underline ring-ring ring-offset-2 ring-offset-destructive-background transition-colors duration-200 hover:text-primary-highlight/80 focus:outline-none focus:ring-1"
+            onClick={() => switchNetwork(PREFERRED_NETWORK_METADATA.id)}
+          >
+            Switch network.
+          </button>
+        ) : (
+          <></>
+        )}
       </Card>
     );
   }
