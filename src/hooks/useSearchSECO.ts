84fca09b62c3fea480a55e7ff1bb324190d64292
Thinus <thijnkroon@gmail.com>
2023-05-09 16:33:57 +0200

    feat: restructure query page

--- src/hooks/useSearchSECO.ts
+++ src/hooks/useSearchSECO.ts
@@ -6,14 +6,15 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { promise } from '@/src/hooks/useToast';
 import { getErrorMessage } from '@/src/lib/utils';
 import { useState } from 'react';
 
 type QueryResponse = any;
 type CheckResponse = any;
 
-type UseSearchSECOProps = {};
+type UseSearchSECOProps = {
+  useDummyData?: boolean;
+};
 
 type UseSearchSECOData = {
   queryResult: QueryResponse;
@@ -21,14 +22,135 @@ type UseSearchSECOData = {
   checkHashes: (hashes: string[]) => Promise<any>;
 };
 
+const dummyQueryReuslt = {
+  methodData: [
+    {
+      method_hash: '9917d1b8a373ac2ac6d92ced37558db2',
+      projectID: '1790983771',
+      startVersion: '1527703251000',
+      startVersionHash: '75e749a7926a3ae2dfd5b2eaab6d15956f73381a',
+      endVersion: '1527797086000',
+      endVersionHash: 'a80ee0d831a8ee69f1fad5b4673491847975eb26',
+      method_name: '',
+      file: 'test.js',
+      lineNumber: '7',
+      parserVersion: '1',
+      vulnCode: '',
+      authorTotal: 'MIT License',
+      authorIds: [
+        '3',
+        '0af1d147-b483-76a7-9e14-7f6828b94a60',
+        '66163fed-c2bd-940d-b4a6-ec6e153a90c4',
+        'a6ad6bc8-a201-ff89-c5d3-0ea9c00a16d0',
+      ],
+    },
+    {
+      method_hash: '897dadeff0b5432633e7f4a8b568fe9f',
+      projectID: '1790983771',
+      startVersion: '1527797086000',
+      startVersionHash: 'a80ee0d831a8ee69f1fad5b4673491847975eb26',
+      endVersion: '1527797086000',
+      endVersionHash: 'a80ee0d831a8ee69f1fad5b4673491847975eb26',
+      method_name: 'isOdd',
+      file: 'index.js',
+      lineNumber: '12',
+      parserVersion: '1',
+      vulnCode: '',
+      authorTotal: 'MIT License',
+      authorIds: [
+        '2',
+        '0af1d147-b483-76a7-9e14-7f6828b94a60',
+        '66163fed-c2bd-940d-b4a6-ec6e153a90c4',
+      ],
+    },
+    {
+      method_hash: '8c5d123198562f030ee15579e08e4224',
+      projectID: '1790983771',
+      startVersion: '1527703251000',
+      startVersionHash: '75e749a7926a3ae2dfd5b2eaab6d15956f73381a',
+      endVersion: '1527797086000',
+      endVersionHash: 'a80ee0d831a8ee69f1fad5b4673491847975eb26',
+      method_name: '',
+      file: 'test.js',
+      lineNumber: '28',
+      parserVersion: '1',
+      vulnCode: '',
+      authorTotal: 'MIT License',
+      authorIds: ['1', '0af1d147-b483-76a7-9e14-7f6828b94a60'],
+    },
+    {
+      method_hash: '16d5818bec817cdab47ed68b07aa511c',
+      projectID: '1790983771',
+      startVersion: '1527703251000',
+      startVersionHash: '75e749a7926a3ae2dfd5b2eaab6d15956f73381a',
+      endVersion: '1527797086000',
+      endVersionHash: 'a80ee0d831a8ee69f1fad5b4673491847975eb26',
+      method_name: '',
+      file: 'test.js',
+      lineNumber: '19',
+      parserVersion: '1',
+      vulnCode: '',
+      authorTotal: 'MIT License',
+      authorIds: ['1', '0af1d147-b483-76a7-9e14-7f6828b94a60'],
+    },
+    {
+      method_hash: '6bac8a660e8db4b32ab77c5fb8682744',
+      projectID: '1790983771',
+      startVersion: '1527703251000',
+      startVersionHash: '75e749a7926a3ae2dfd5b2eaab6d15956f73381a',
+      endVersion: '1527797086000',
+      endVersionHash: 'a80ee0d831a8ee69f1fad5b4673491847975eb26',
+      method_name: '',
+      file: 'test.js',
+      lineNumber: '8',
+      parserVersion: '1',
+      vulnCode: '',
+      authorTotal: 'MIT License',
+      authorIds: ['1', '0af1d147-b483-76a7-9e14-7f6828b94a60'],
+    },
+  ],
+  authorData: [
+    {
+      username: 'Error parsing author id: 3',
+    },
+  ],
+  projectData: [
+    {
+      id: '1790983771',
+      versionTime: '1527703251000',
+      versionHash: '75e749a7926a3ae2dfd5b2eaab6d15956f73381a',
+      license: 'MIT License',
+      name: 'is-odd',
+      url: 'https://github.com/i-voted-for-trump/is-odd',
+      authorName: 'a601fbd4-9cbd-62d8-2587-fd60d02e296b',
+      authorMail: '1',
+    },
+    {
+      id: '1790983771',
+      versionTime: '1527797086000',
+      versionHash: 'a80ee0d831a8ee69f1fad5b4673491847975eb26',
+      license: 'MIT License',
+      name: 'is-odd',
+      url: 'https://github.com/i-voted-for-trump/is-odd',
+      authorName: 'a601fbd4-9cbd-62d8-2587-fd60d02e296b',
+      authorMail: '1',
+    },
+  ],
+};
+
 /**
  * Utility functions to interact with the SearchSECO database and API
  * @returns Functions and results of interacting with the SearchSECO database and API
  */
-export const useSearchSECO = (props: UseSearchSECOProps): UseSearchSECOData => {
+export const useSearchSECO = ({
+  useDummyData,
+}: UseSearchSECOProps): UseSearchSECOData => {
   const [queryResult, setQueryResult] = useState<CheckResponse | null>(null);
 
-  const runQuery = async (url: string, token: string): Promise<QueryResponse> => {
+  const runQuery = async (
+    url: string,
+    token: string
+  ): Promise<QueryResponse> => {
     try {
       const response = await fetch('http://localhost:8080/api/fetch', {
         method: 'POST',
--- src/pages/Query.tsx
+++ src/pages/Query.tsx
@@ -50,10 +50,12 @@ const Query = () => {
   } = useForm<QueryFormData>();
   const { queryResult, runQuery, checkHashes } = useSearchSECO({});
   const [hashCount, setHashCount] = useState<number | null>(null);
+  const { isConnected } = useAccount();
 
-  const { address } = useAccount();
-
-  const [storedToken, setStoredToken] = useLocalStorage<string>('githubAccessToken', '');
+  const [storedToken, setStoredToken] = useLocalStorage<string>(
+    'githubAccessToken',
+    ''
+  );
 
   const downloadResults = () => {
     if (queryResult) {
@@ -75,8 +77,8 @@ const Query = () => {
         return checkHashes(hashes);
       }),
       {
-        loading: 'Querying SearchSECO database and retrieving hash metadata...',
-        success: 'Hash metadata retrieved successfully!',
+        loading: 'Querying SearchSECO database...',
+        success: 'Query successful!',
         error: (err) => ({
           title: err,
           description: '',
@@ -88,121 +90,127 @@ const Query = () => {
   return (
     <div className="space-y-6">
       <HeaderCard title="SearchSECO" />
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-        <div className="space-y-6">
-          <MainCard header="Query" icon={HiOutlineDocumentMagnifyingGlass}>
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="flex flex-col gap-2"
-            >
-              <div className="space-y-1">
-                <Label
-                  htmlFor="searchUrl"
-                  tooltip="Github URL to the repository to query the SearchSECO database for"
-                >
-                  Github URL
-                </Label>
-                <ErrorWrapper name="URL" error={errors.searchUrl}>
-                  <Input
-                    {...register('searchUrl', {
-                      required: true,
-                      pattern: {
-                        value: UrlPattern,
-                        message: 'Invalid URL',
-                      },
-                    })}
-                    type="text"
-                    placeholder="https://github.com/..."
-                    id="searchUrl"
-                    aria-invalid={errors.searchUrl ? 'true' : 'false'}
-                    error={errors.searchUrl}
-                  />
-                </ErrorWrapper>
-              </div>
-              <div className="space-y-1">
-                <Label
-                  htmlFor="token"
-                  tooltip="Your Github access token will be used to download the repository to know what to fetch from the SearchSECO database. Only read access is required for the access token."
-                >
-                  Github access token
-                </Label>
-                <ErrorWrapper name="Token" error={errors.token}>
-                  <Input
-                    {...register('token', {
-                      required: true,
-                      pattern: {
-                        value: /^[\w\d-]+$/, //placeholder
-                        message: 'Invalid token',
-                      },
-                    })}
-                    type="text"
-                    placeholder="Your Github token"
-                    id="token"
-                    aria-invalid={errors.token ? 'true' : 'false'}
-                    error={errors.token}
-                    defaultValue={storedToken}
-                  />
-                </ErrorWrapper>
-              </div>
-              <Button type="submit" className="" 
-              //disabled={!address}           Commented for testing purposes
+      <div className="grid grid-cols-7 gap-6">
+        <MainCard
+          header="Query"
+          className="col-span-full lg:col-span-3"
+          icon={HiOutlineDocumentMagnifyingGlass}
+        >
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-2"
+          >
+            <div className="space-y-1">
+              <Label
+                htmlFor="searchUrl"
+                tooltip="Github URL to the repository to query the SearchSECO database for"
               >
+                Github URL
+              </Label>
+              <ErrorWrapper name="URL" error={errors.searchUrl}>
+                <Input
+                  {...register('searchUrl', {
+                    required: true,
+                    pattern: {
+                      value: UrlPattern,
+                      message: 'Invalid URL',
+                    },
+                  })}
+                  type="text"
+                  placeholder="https://github.com/..."
+                  id="searchUrl"
+                  aria-invalid={errors.searchUrl ? 'true' : 'false'}
+                  error={errors.searchUrl}
+                />
+              </ErrorWrapper>
+            </div>
+            <div className="space-y-1">
+              <Label
+                htmlFor="token"
+                tooltip="Your Github access token will be used to download the repository to know what to fetch from the SearchSECO database. Only read access is required for the access token."
+              >
+                Github access token
+              </Label>
+              <ErrorWrapper name="Token" error={errors.token}>
+                <Input
+                  {...register('token', {
+                    required: true,
+                    pattern: {
+                      value: /^[\w\d-]+$/, //placeholder
+                      message: 'Invalid token',
+                    },
+                  })}
+                  type="text"
+                  placeholder="Your Github token"
+                  id="token"
+                  aria-invalid={errors.token ? 'true' : 'false'}
+                  error={errors.token}
+                  defaultValue={storedToken}
+                />
+              </ErrorWrapper>
+            </div>
+            <div className="flex flex-row items-center gap-x-4">
+              <Button type="submit" className="" disabled={!isConnected}>
                 Submit
               </Button>
-              {!address && <ConnectWalletWarning action="to query the SearchSECO database" />}
-            </form>
-          </MainCard>
-          <MainCard header="Result" icon={HiOutlineCodeBracketSquare}>
-            {hashCount !== null ? (
-              <>
-                <p>
-                  Number of hashes found: <strong>{hashCount}</strong>
-                </p>
-                {hashCount > 0 ? (
-                  <>
-                    {queryResult && queryResult.methodData ? (
-                      <>
-                        <Table
-                          columns={[
-                            { header: 'Hash', accessor: 'method_hash' },
-                            { header: 'File Name', accessor: 'file' },
-                            { header: 'Function Name', accessor: 'method_name' },
-                            { header: 'Line Number', accessor: 'lineNumber' },
-                          ]}
-                          data={queryResult.methodData}
-                        />
-                        <Button
-                          onClick={downloadResults}
-                          className="mt-2"
-                          disabled={!queryResult}
-                          icon={HiArrowDownTray}
-                        >
-                          Download as JSON file
-                        </Button>
-                      </>
-                    ) : (
-                      <p className="text-base font-normal italic text-highlight-foreground/80">
-                        Loading metadata...
-                      </p>
-                    )}
-                  </>
-                ) : (
-                  <p className="text-base font-normal italic text-highlight-foreground/80">
-                    No hashes found
-                  </p>
-                )}
-              </>
-            ) : (
-              <p className="text-base font-normal italic text-highlight-foreground/80">
-                Query results will be displayed here
+              {!isConnected && (
+                <ConnectWalletWarning action="to query the database" />
+              )}
+            </div>
+          </form>
+        </MainCard>
+        <MainCard
+          header="Result"
+          className="col-span-full lg:col-span-4"
+          icon={HiOutlineCodeBracketSquare}
+        >
+          {hashCount !== null ? (
+            <>
+              <p>
+                Number of hashes found: <strong>{hashCount}</strong>
               </p>
-            )}
-          </MainCard>
-        </div>
-        <MainCard header="Rewards" icon={HiOutlineCurrencyDollar}>
-          <p className="text-base font-normal italic text-highlight-foreground/80">
-            This card will be used for claiming rewards
-          </p>
+              {hashCount > 0 ? (
+                <>
+                  {queryResult && queryResult.methodData ? (
+                    <>
+                      <Table
+                        columns={[
+                          { header: 'Hash', accessor: 'method_hash' },
+                          { header: 'File Name', accessor: 'file' },
+                          {
+                            header: 'Function Name',
+                            accessor: 'method_name',
+                          },
+                          { header: 'Line Number', accessor: 'lineNumber' },
+                        ]}
+                        data={queryResult.methodData}
+                      />
+                      <Button
+                        onClick={downloadResults}
+                        className="mt-2"
+                        disabled={!queryResult}
+                        icon={HiArrowDownTray}
+                      >
+                        Download as JSON file
+                      </Button>
+                    </>
+                  ) : (
+                    <p className="text-base font-normal italic text-highlight-foreground/80">
+                      Loading metadata...
+                    </p>
+                  )}
+                </>
+              ) : (
+                <p className="text-base font-normal italic text-highlight-foreground/80">
+                  No hashes found
+                </p>
+              )}
+            </>
+          ) : (
+            <p className="text-base font-normal italic text-highlight-foreground/80">
+              Query results will be displayed here
+            </p>
+          )}
         </MainCard>
       </div>
     </div>
