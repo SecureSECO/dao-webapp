9b0947132903cd2f6fd16cf4e80b33cf9d0460d0
Hidde-Heijnen <hiddeh11@gmail.com>
2023-05-15 15:43:12 +0200

    feat: better nav dropdown

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -18,11 +18,31 @@ import {
   DropdownMenuTrigger,
 } from '@/src/components/ui/Dropdown';
 import { cn } from '@/src/lib/utils';
-import { HiBars3, HiChevronDown, HiXMark } from 'react-icons/hi2';
+import { createElement } from 'react';
+import { IconType } from 'react-icons';
+import { FaDiscord } from 'react-icons/fa';
+import { HiOutlineTerminal } from 'react-icons/hi';
+import {
+  HiBars3,
+  HiChevronDown,
+  HiDocumentMagnifyingGlass,
+  HiOutlineDocumentMagnifyingGlass,
+  HiOutlineGlobeAlt,
+  HiXMark,
+} from 'react-icons/hi2';
 import { NavLink, useLocation } from 'react-router-dom';
 
 type NavItemPage = { label: string; url: string };
-type NavItemCollection = { label: string; pages: NavItemPage[] };
+type NavItemPageWithIcon = NavItemPage & {
+  icon: IconType;
+  description?: string;
+};
+type NavItemCollection = {
+  label: string;
+  pages: NavItemPageWithIcon[];
+  alternativeLinks?: NavItemPageWithIcon[];
+};
+
 type NavItem = NavItemPage | NavItemCollection;
 
 const navItems: NavItem[] = [
@@ -45,8 +65,22 @@ const navItems: NavItem[] = [
   {
     label: 'SearchSECO',
     pages: [
-      { label: 'Query', url: '/query' },
-      { label: 'Mining', url: '/mining' },
+      {
+        label: 'Query',
+        url: '/query',
+        icon: HiOutlineDocumentMagnifyingGlass,
+        description: 'Query the SearchSECO database using your $SECOINS',
+      },
+      {
+        label: 'Mining',
+        url: '/mining',
+        icon: HiOutlineTerminal,
+        description: 'Claim your mining rewards in $SECOINS or $SECOREP',
+      },
+    ],
+    alternativeLinks: [
+      { label: 'Website', url: '#', icon: HiOutlineGlobeAlt },
+      { label: 'Discord', url: '#', icon: FaDiscord },
     ],
   },
   {
@@ -62,8 +96,8 @@ const Navitempage = ({ item }: { item: NavItemPage }) => {
       to={item.url}
       className={({ isActive, isPending }) =>
         cn(
-          'w-full rounded-md px-4 py-2 text-lg font-semibold ring-ring ring-offset-2 ring-offset-background focus:outline-none focus:ring-2',
-          isActive && 'active bg-highlight text-primary shadow-md',
+          'w-full select-none rounded-md px-4 py-2 text-lg font-semibold ring-ring ring-offset-2 ring-offset-background hover:bg-highlight/50 focus:outline-none focus:ring-2',
+          isActive && 'bg-highlight text-primary shadow-md',
           isPending && ''
         )
       }
@@ -81,21 +115,59 @@ const Navitemcollection = ({ item }: { item: NavItemCollection }) => {
     <DropdownMenu>
       <DropdownMenuTrigger
         className={cn(
-          'flex w-full flex-row items-center gap-x-1 rounded-md px-4 py-2 text-lg font-semibold leading-4 ring-ring ring-offset-2 ring-offset-background hover:cursor-pointer focus:outline-none focus:ring-2',
-          isActive && 'active bg-highlight text-primary shadow-md'
+          'flex w-full select-none items-center justify-center gap-x-1 rounded-md px-4 py-2 text-lg font-semibold leading-4 ring-ring ring-offset-2 ring-offset-background hover:cursor-pointer hover:bg-highlight/50 focus:outline-none focus:ring-2 data-[state=open]:bg-highlight/50',
+          isActive && 'bg-highlight text-primary shadow-md '
         )}
       >
         {item.label}
-        <HiChevronDown className="mt-1 h-5 w-5 group-data-[state=open]:hidden" />
-        <HiXMark className="mt-1 h-5 w-5 group-data-[state=closed]:hidden" />
+        <HiChevronDown className="transi h-5 w-5 transition-transform group-data-[state=open]:rotate-180" />
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="absolute -left-6 origin-top">
-        <DropdownMenuGroup>
+      <DropdownMenuContent className="w-screen max-w-xs origin-top flex-col divide-popover-foreground/10 rounded-3xl p-0 text-sm leading-5 shadow-lg">
+        <DropdownMenuGroup className="p-2">
           {item.pages.map((page) => (
-            <DropdownMenuItem key={page.label} className="hover:cursor-pointer">
-              <Navitem item={page} />
-            </DropdownMenuItem>
+            <div
+              key={page.label}
+              className={cn(
+                location.pathname == page.url
+                  ? 'text-primary dark:text-primary-highlight'
+                  : '',
+                'group relative flex gap-x-6 rounded-lg p-4 transition-colors hover:bg-popover-foreground/5'
+              )}
+            >
+              <div
+                className={cn(
+                  location.pathname == page.url
+                    ? 'bg-primary text-primary-foreground'
+                    : 'bg-popover-foreground/5 group-hover:bg-popover group-hover:text-primary',
+                  'mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg '
+                )}
+              >
+                <page.icon className="h-6 w-6" aria-hidden="true" />
+              </div>
+              <div className="flex flex-col gap-0.5">
+                <NavLink to={page.url}>
+                  <span className=" font-semibold">{page.label}</span>
+                  <p className="opacity-80">{page.description}</p>
+                </NavLink>
+              </div>
+            </div>
           ))}
+        </DropdownMenuGroup>
+        <DropdownMenuGroup className="grid h-full w-full grid-cols-2 divide-x divide-popover-foreground/10 bg-popover-foreground/5">
+          {item.alternativeLinks &&
+            item.alternativeLinks.map((item) => (
+              <NavLink
+                key={item.label}
+                to={item.url}
+                className="flex items-center justify-center gap-x-2.5 p-3 font-semibold hover:bg-popover-foreground/20"
+              >
+                <item.icon
+                  className="h-5 w-5 flex-none opacity-80"
+                  aria-hidden="true"
+                />
+                {item.label}
+              </NavLink>
+            ))}
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
