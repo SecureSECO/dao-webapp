ac8ed20891386b8743a2484d2fea37e46750a658
Thinus <thijnkroon@gmail.com>
2023-05-25 16:06:28 +0200

    fix: layout of WithdrawAssets input

--- src/components/newProposal/actions/WithdrawAssetsInput.tsx
+++ src/components/newProposal/actions/WithdrawAssetsInput.tsx
@@ -156,11 +156,15 @@ export const WithdrawAssetsInput = () => {
             placeholder="0x..."
           />
         </ErrorWrapper>
-        {address === 'custom' &&
-          'Please note that if the requisted assets are not present in the treasury at the time of withdrawal, this action will fail.'}
       </div>
-      <div className="grid grid-cols-1 gap-x-2 gap-y-4 sm:grid-cols-2">
-        <div className="flex flex-col gap-y-1 ">
+      {address === 'custom' && (
+        <p className="text-sm text-popover-foreground/80">
+          Note that if the requisted assets are not present in the treasury at
+          the time of withdrawal, this action will fail.
+        </p>
+      )}
+      <div className="grid grid-cols-1 items-end gap-2 sm:grid-cols-2">
+        <div className="space-y-1">
           <Label tooltip="Token to withdraw" htmlFor="tokenAddress">
             Token
           </Label>
@@ -220,132 +224,133 @@ export const WithdrawAssetsInput = () => {
               </Select>
             )}
           />
-
-          <ErrorWrapper
-            className={cn(address === 'custom' || 'hidden')}
-            name="tokenAddressCustom"
-            error={errors?.tokenAddressCustom}
-          >
-            <Label
-              tooltip="The contract address of the token"
-              htmlFor="tokenAddressCustom"
-            >
-              Token address
-            </Label>
-            <Input
-              error={errors?.tokenAddressCustom}
-              placeholder="0x..."
-              {...register(`${prefix}.tokenAddressCustom`, {
-                validate: (v) => {
-                  if (address == 'custom') {
-                    const valid =
-                      v === undefined ? false : AddressPattern.test(v);
-                    return (
-                      valid ||
-                      'Please enter an address starting with 0x, followed by 40 address characters'
-                    );
-                  }
-                },
-              })}
-            />
-          </ErrorWrapper>
-          <ErrorWrapper
-            //Only show when custom NFT tokens are shown
-            className={cn(tokenType === TokenType.ERC721 || 'hidden')}
-            name="tokenID"
-            error={errors?.tokenID}
-          >
-            <Label
-              tooltip="The token ID of the NFT (ERC721/ERC1155)"
-              htmlFor="tokenID"
-            >
-              Token ID
-            </Label>
-            <Input
-              error={errors?.tokenID}
-              placeholder="123..."
-              {...register(`${prefix}.tokenID`, {
-                validate: (v) => {
-                  if (tokenType === TokenType.ERC721) {
-                    const empty = v === '' || v === undefined || v === null;
-                    const valid = !empty && IntegerPattern.test(v);
-
-                    return (
-                      valid ||
-                      'Please enter a valid token ID, i.e. a number like 123'
-                    );
-                  }
-                  // Not NFT token, thus valid
-                  return true;
-                },
-              })}
-            />
-          </ErrorWrapper>
         </div>
-        <div className="flex flex-col gap-y-1">
+
+        <ErrorWrapper name="Amount" error={errors?.amount}>
           <Label tooltip="Amount of tokens to withdraw" htmlFor="amount">
             Amount
           </Label>
-          <ErrorWrapper name="Amount" error={errors?.amount}>
-            <Input
-              {...register(`${prefix}.amount`, {
-                required: true,
-                pattern: {
-                  value: NumberPattern,
-                  message: 'Please enter a number, e.g. 3.141',
-                },
-              })}
-              type="string"
-              id="amount"
-              placeholder="0"
-              disabled={tokenType === TokenType.ERC721}
-              min="0"
-              error={errors?.amount}
-            />
-          </ErrorWrapper>
-          <ErrorWrapper
-            className={cn(address === 'custom' || 'hidden')}
-            name="tokenType"
-            error={errors?.tokenType}
+          <Input
+            {...register(`${prefix}.amount`, {
+              required: true,
+              pattern: {
+                value: NumberPattern,
+                message: 'Please enter a number, e.g. 3.141',
+              },
+            })}
+            type="string"
+            id="amount"
+            placeholder="0"
+            disabled={tokenType === TokenType.ERC721}
+            min="0"
+            error={errors?.amount}
+          />
+        </ErrorWrapper>
+
+        <ErrorWrapper
+          className={cn(address === 'custom' || 'hidden')}
+          name="tokenAddressCustom"
+          error={errors?.tokenAddressCustom}
+        >
+          <Label
+            tooltip="The contract address of the token"
+            htmlFor="tokenAddressCustom"
           >
-            <Label
-              tooltip="The contract address type of the token"
-              htmlFor="tokenType"
-            >
-              Token type
-            </Label>
-            <Controller
-              control={control}
-              name={`${prefix}.tokenType`}
-              render={({ field: { onChange, name, value } }) => (
-                <Select
-                  onValueChange={(v) => {
-                    onChange(v);
-                    if (v === TokenType.ERC721) {
-                      setValue(`${prefix}.amount`, '1');
-                    }
-                  }}
-                  defaultValue={value}
-                  name={name}
-                  required
-                >
-                  <SelectTrigger>
-                    <SelectValue placeholder="Token type" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      {tokenTypesInfo.map((token, i) => (
-                        <SelectItem key={i} value={token.type}>
-                          {token.displayName}
-                        </SelectItem>
-                      ))}
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-              )}
-            />
-          </ErrorWrapper>
-        </div>
+            Token address
+          </Label>
+          <Input
+            error={errors?.tokenAddressCustom}
+            placeholder="0x..."
+            {...register(`${prefix}.tokenAddressCustom`, {
+              validate: (v) => {
+                if (address == 'custom') {
+                  const valid =
+                    v === undefined ? false : AddressPattern.test(v);
+                  return (
+                    valid ||
+                    'Please enter an address starting with 0x, followed by 40 address characters'
+                  );
+                }
+              },
+            })}
+          />
+        </ErrorWrapper>
+
+        <ErrorWrapper
+          //Only show when custom NFT tokens are shown
+          className={cn(tokenType === TokenType.ERC721 || 'hidden')}
+          name="tokenID"
+          error={errors?.tokenID}
+        >
+          <Label
+            tooltip="The token ID of the NFT (ERC721/ERC1155)"
+            htmlFor="tokenID"
+          >
+            Token ID
+          </Label>
+          <Input
+            error={errors?.tokenID}
+            placeholder="123..."
+            {...register(`${prefix}.tokenID`, {
+              validate: (v) => {
+                if (tokenType === TokenType.ERC721) {
+                  const empty = v === '' || v === undefined || v === null;
+                  const valid = !empty && IntegerPattern.test(v);
+
+                  return (
+                    valid ||
+                    'Please enter a valid token ID, i.e. a number like 123'
+                  );
+                }
+                // Not NFT token, thus valid
+                return true;
+              },
+            })}
+          />
+        </ErrorWrapper>
+
+        <ErrorWrapper
+          className={cn(address === 'custom' || 'hidden')}
+          name="tokenType"
+          error={errors?.tokenType}
+        >
+          <Label
+            tooltip="The contract address type of the token"
+            htmlFor="tokenType"
+          >
+            Token type
+          </Label>
+          <Controller
+            control={control}
+            name={`${prefix}.tokenType`}
+            render={({ field: { onChange, name, value } }) => (
+              <Select
+                onValueChange={(v) => {
+                  onChange(v);
+                  if (v === TokenType.ERC721) {
+                    setValue(`${prefix}.amount`, '1');
+                  }
+                }}
+                defaultValue={value}
+                name={name}
+                required
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Token type" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {tokenTypesInfo.map((token, i) => (
+                      <SelectItem key={i} value={token.type}>
+                        {token.displayName}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+          />
+        </ErrorWrapper>
       </div>
     </MainCard>
   );
