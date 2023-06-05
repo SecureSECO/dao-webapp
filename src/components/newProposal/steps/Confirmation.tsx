8fd3763d34add14260923d572942d66153fd02db
Thinus <thijnkroon@gmail.com>
2023-06-05 10:16:10 +0200

    Revert "chore: replace ChatBubble icon with outline version"
    This reverts commit b0ae03d4dfdaf4ff83dd0188536e5708ce802fc1.

--- src/components/newProposal/steps/Confirmation.tsx
+++ src/components/newProposal/steps/Confirmation.tsx
@@ -40,7 +40,7 @@ import { Action } from '@plopmenz/diamond-governance-sdk';
 import { add, format } from 'date-fns';
 import DOMPurify from 'dompurify';
 import { useForm } from 'react-hook-form';
-import { HiOutlineChatBubbleLeftRight } from 'react-icons/hi2';
+import { HiChatBubbleLeftRight } from 'react-icons/hi2';
 import { useNavigate } from 'react-router';
 import { useAccount } from 'wagmi';
 
@@ -283,7 +283,7 @@ export const Confirmation = () => {
 
         {/* View voting settings */}
         <MainCard
-          icon={HiOutlineChatBubbleLeftRight}
+          icon={HiChatBubbleLeftRight}
           variant="outline"
           header="Voting settings"
         >
