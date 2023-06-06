af59eedcbd13c7d2d7d0de88a50c9426d9ad3f7a
BenjaminMeijs <benjamin.meijs@gmail.com>
2023-06-06 12:11:51 +0200

    feat: add WhitelistAction

---
+++ src/components/proposal/actions/WhitelistAction.stories.tsx
@@ -0,0 +1,56 @@
+/**
+ * This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
+ * © Copyright Utrecht University (Department of Information and Computing Sciences)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Accordion } from '@/src/components/ui/Accordion';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { WhitelistAction } from './WhitelistAction';
+
+const meta = {
+  component: WhitelistAction,
+  argTypes: {
+    action: {
+      table: {
+        disable: true,
+      },
+    },
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+    value: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} satisfies Meta<typeof WhitelistAction>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    value: 'first',
+    action: {
+      method: 'whitelist(address)',
+      interface: 'IMembershipWhitelisting',
+      params: {
+        _address: '0x123456789009876543211234567890',
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Accordion type="single" collapsible>
+        <Story />
+      </Accordion>
+    ),
+  ],
+};
