3f55198e427b2caca11b4ae6167714aa8e56c80d
Thinus <thijnkroon@gmail.com>
2023-06-06 14:30:08 +0200

    feat: increase test timeout for storybook to 30s (from 15s)

---
+++ test-runner-jest.config.js
@@ -0,0 +1,22 @@
+/**
+ * This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
+ * © Copyright Utrecht University (Department of Information and Computing Sciences)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * This file is used by the Storybook Test Runner to configure Jest
+ */
+
+const { getJestConfig } = require('@storybook/test-runner');
+
+module.exports = {
+  // The default configuration comes from @storybook/test-runner
+  ...getJestConfig(),
+  /** Add your own overrides below
+   * @see https://jestjs.io/docs/configuration
+   */
+  testTimeout: 30000,
+};
