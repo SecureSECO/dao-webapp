6cac7c2e947049a9fefde2688d72092df4d31bfe
Hidde Heijnen <h.r.a.heijnen@students.uu.nl>
2023-06-11 22:43:23 +0200

    feat: RHF

--- src/pages/Swap.tsx
+++ src/pages/Swap.tsx
@@ -12,6 +12,7 @@ import { Label } from '@/src/components/ui/Label';
 import { MainCard } from '@/src/components/ui/MainCard';
 import { useDiamondSDKContext } from '@/src/context/DiamondGovernanceSDK';
 import { TOKENS } from '@/src/lib/constants/tokens';
+import { useForm } from 'react-hook-form';
 import { HiArrowsRightLeft, HiOutlineArrowsUpDown } from 'react-icons/hi2';
 import { useAccount } from 'wagmi';
 
@@ -36,7 +37,23 @@ const Icon = ({ name }: { name: string }) => {
   }
 };
 
+interface IFormInputs {
+  fromToken: number;
+}
+
 const Swap = () => {
+  const { register, handleSubmit, setValue } = useForm<IFormInputs>();
+
+  const onSubmit = (data: IFormInputs) => {
+    console.log(data);
+  };
+
+  const max = 100;
+
+  const setMaxValue = () => {
+    setValue('fromToken', 100);
+  };
+
   const { secoinAddress } = useDiamondSDKContext();
   const { address, isConnected } = useAccount();
 
@@ -51,11 +68,12 @@ const Swap = () => {
         icon={HiArrowsRightLeft}
         className="max-w-[40rem] relative" // Add relative here
       >
-        <form className="space-y-2">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-2">
           {/* From token */}
           <Label>From:</Label>
           <div className="flex p-4 h-24 bg-popover text-popover-foreground rounded-md border border-input">
             <Input
+              {...register('fromToken')}
               type="number"
               className="border-none text-2xl [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none [appearance:textfield] focus:ring-0 focus:ring-offset-0"
               min={'0.000000000000000001'}
@@ -67,11 +85,11 @@ const Swap = () => {
                 <Icon name={swap ? abcTokens[1].name : abcTokens[0].name} />
                 {swap ? abcTokens[1].name : abcTokens[0].name}
               </div>
-              <MaxButton address={address} />
+              <MaxButton max={max} setMaxValue={setMaxValue} />
             </div>
           </div>
           {/* Swap button */}
-          <div className="absolute top-1/2 -translate-y- left-1/2 -translate-y-5 -translate-x-1/2">
+          <div className="absolute top-1/2 left-1/2 -translate-y-2 -translate-x-1/2">
             <Button
               onClick={() => {
                 setSwap(!swap);
@@ -120,12 +138,19 @@ const Swap = () => {
 };
 export default Swap;
 
-const MaxButton = ({ address }: { address: `0x${string}` | undefined }) => {
+const MaxButton = ({
+  max,
+  setMaxValue,
+}: {
+  max: number;
+  setMaxValue: () => void;
+}) => {
   return (
     <div className="inline-flex items-center justify-center gap-x-1">
-      123234234
+      {max}
       <button
         type="button"
+        onClick={setMaxValue}
         className="w-full p-1 h-fit text-blue-500 underline underline-offset-2 active:scale-95 hover:text-blue-300"
       >
         Max
