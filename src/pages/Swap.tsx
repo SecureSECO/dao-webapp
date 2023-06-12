3e8379f302565e3a58e581a33c87169fcae4e80b
BenjaminMeijs <benjamin.meijs@gmail.com>
2023-06-12 14:38:38 +0200

    chore: add license to files

--- src/pages/Swap.tsx
+++ src/pages/Swap.tsx
@@ -1,4 +1,10 @@
-import { useState } from 'react';
+/**
+ * This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
+ * © Copyright Utrecht University (Department of Information and Computing Sciences)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+import { useEffect, useState } from 'react';
 import DAI from '@/src/components/icons/DAI';
 import Secoin from '@/src/components/icons/Secoin';
 import { Button } from '@/src/components/ui/Button';
